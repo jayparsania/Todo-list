517214977a8a6eb3554e9cb816526a2a8706e008
Jay Parsania <jay.parsania333@gmail.com>
2023-12-16 16:07:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const todoList = [];
+
+renderTodoList();
+
+function renderTodoList() {
+
+    let todoListHtml = '';
+
+    todoList.forEach(function(todoObject, index){
+
+        const { name, dueDate } = todoObject;
+        const html = `
+            <div>${name}</div>
+            <div>${dueDate}</div>
+                    
+            <button onclick = "
+            todoList.splice(${index},1);
+            renderTodoList();
+            " class="delete-todo-btn">Delete</button>
+            
+        `;
+        todoListHtml += html;
+    })
+
+    console.log(todoListHtml);
+
+    document.querySelector('.js-todo-list').innerHTML = todoListHtml
+
+}
+
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+
+    const dateInputElement = document.querySelector('.js-due-date-input');
+
+    const dueDate = dateInputElement.value;
+
+    if(name){
+        todoList.push({
+            name,
+            dueDate
+        });
+
+    }
+   
+
+    inputElement.value = '';
+
+    renderTodoList();
+}
+
